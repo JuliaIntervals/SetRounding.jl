5fb88694f64964873db50831b2b2069af3a0bc71
David Sanders <dpsanders@gmail.com>
2018-08-11 17:00:28 +0100

    Julia 0.6 version; add noinline

--- src/SetRounding.jl
+++ src/SetRounding.jl
@@ -1,8 +1,17 @@
 module SetRounding
 # Extracted from Julia base. License is MIT: https://julialang.org/license
 
-let fenv_consts = Vector{Cint}(undef, 9)
-    ccall(:jl_get_fenv_consts, Cvoid, (Ptr{Cint},), fenv_consts)
+# This file is a part of Julia. License is MIT: https://julialang.org/license
+
+import Base:
+    RoundingMode, RoundNearest, RoundToZero, RoundUp, RoundDown, RoundFromZero,
+    RoundNearestTiesAway, RoundNearestTiesUp,
+    rounding, setrounding,
+    get_zero_subnormals, set_zero_subnormals
+
+
+let fenv_consts = Vector{Cint}(9)
+    ccall(:jl_get_fenv_consts, Void, (Ptr{Cint},), fenv_consts)
     global const JL_FE_INEXACT = fenv_consts[1]
     global const JL_FE_UNDERFLOW = fenv_consts[2]
     global const JL_FE_OVERFLOW = fenv_consts[3]
@@ -15,92 +24,6 @@ let fenv_consts = Vector{Cint}(undef, 9)
     global const JL_FE_TOWARDZERO = fenv_consts[9]
 end
 
-
-import Base:
-    RoundingMode, RoundNearest, RoundToZero, RoundUp, RoundDown, RoundFromZero,
-    RoundNearestTiesAway, RoundNearestTiesUp,
-    rounding, setrounding,
-    get_zero_subnormals, set_zero_subnormals, Cvoid
-
-## rounding modes ##
-"""
-    RoundingMode
-
-A type used for controlling the rounding mode of floating point operations (via
-[`rounding`](@ref)/[`setrounding`](@ref) functions), or as
-optional arguments for rounding to the nearest integer (via the [`round`](@ref)
-function).
-
-Currently supported rounding modes are:
-
-- [`RoundNearest`](@ref) (default)
-- [`RoundNearestTiesAway`](@ref)
-- [`RoundNearestTiesUp`](@ref)
-- [`RoundToZero`](@ref)
-- [`RoundFromZero`](@ref) ([`BigFloat`](@ref) only)
-- [`RoundUp`](@ref)
-- [`RoundDown`](@ref)
-"""
-struct RoundingMode{T} end
-
-"""
-    RoundNearest
-
-The default rounding mode. Rounds to the nearest integer, with ties (fractional values of
-0.5) being rounded to the nearest even integer.
-"""
-const RoundNearest = RoundingMode{:Nearest}()
-
-"""
-    RoundToZero
-
-[`round`](@ref) using this rounding mode is an alias for [`trunc`](@ref).
-"""
-const RoundToZero = RoundingMode{:ToZero}()
-
-"""
-    RoundUp
-
-[`round`](@ref) using this rounding mode is an alias for [`ceil`](@ref).
-"""
-const RoundUp = RoundingMode{:Up}()
-
-"""
-    RoundDown
-
-[`round`](@ref) using this rounding mode is an alias for [`floor`](@ref).
-"""
-const RoundDown = RoundingMode{:Down}()
-
-"""
-    RoundFromZero
-
-Rounds away from zero.
-This rounding mode may only be used with `T == BigFloat` inputs to [`round`](@ref).
-
-# Examples
-```jldoctest
-julia> BigFloat("1.0000000000000001", 5, RoundFromZero)
-1.06
-```
-"""
-const RoundFromZero = RoundingMode{:FromZero}() # mpfr only
-
-"""
-    RoundNearestTiesAway
-
-Rounds to nearest integer, with ties rounded away from zero (C/C++
-[`round`](@ref) behaviour).
-"""
-const RoundNearestTiesAway = RoundingMode{:NearestTiesAway}()
-
-"""
-    RoundNearestTiesUp
-
-Rounds to nearest integer, with ties rounded toward positive infinity (Java/JavaScript
-[`round`](@ref) behaviour).
-"""
-const RoundNearestTiesUp = RoundingMode{:NearestTiesUp}()
 
 to_fenv(::RoundingMode{:Nearest}) = JL_FE_TONEAREST
 to_fenv(::RoundingMode{:ToZero}) = JL_FE_TOWARDZERO
@@ -123,24 +46,24 @@ end
 
 """
     setrounding(T, mode)
-
 Set the rounding mode of floating point type `T`, controlling the rounding of basic
 arithmetic functions ([`+`](@ref), [`-`](@ref), [`*`](@ref),
 [`/`](@ref) and [`sqrt`](@ref)) and type conversion. Other numerical
 functions may give incorrect or invalid values when using rounding modes other than the
-default [`RoundNearest`](@ref).
-
-Note that this is currently only supported for `T == BigFloat`.
+default `RoundNearest`.
+Note that this may affect other types, for instance changing the rounding mode of
+[`Float64`](@ref) will change the rounding mode of [`Float32`](@ref).
+See [`RoundingMode`](@ref) for available modes.
+!!! warning
+    This feature is still experimental, and may give unexpected or incorrect values.
 """
 setrounding(T::Type, mode)
 
 """
     rounding(T)
-
 Get the current floating point rounding mode for type `T`, controlling the rounding of basic
 arithmetic functions ([`+`](@ref), [`-`](@ref), [`*`](@ref), [`/`](@ref)
 and [`sqrt`](@ref)) and type conversion.
-
 See [`RoundingMode`](@ref) for available modes.
 """
 :rounding
@@ -148,20 +71,34 @@ See [`RoundingMode`](@ref) for available modes.
 setrounding_raw(::Type{<:Union{Float32,Float64}}, i::Integer) = ccall(:fesetround, Int32, (Int32,), i)
 rounding_raw(::Type{<:Union{Float32,Float64}}) = ccall(:fegetround, Int32, ())
 
+@noinline setrounding(::Type{T}, r::RoundingMode) where {T<:Union{Float32,Float64}} = setrounding_raw(T,to_fenv(r))
 rounding(::Type{T}) where {T<:Union{Float32,Float64}} = from_fenv(rounding_raw(T))
 
 """
     setrounding(f::Function, T, mode)
-
 Change the rounding mode of floating point type `T` for the duration of `f`. It is logically
 equivalent to:
-
     old = rounding(T)
     setrounding(T, mode)
     f()
     setrounding(T, old)
-
 See [`RoundingMode`](@ref) for available rounding modes.
+!!! warning
+    This feature is still experimental, and may give unexpected or incorrect values. A
+    known problem is the interaction with compiler optimisations, e.g.
+        julia> setrounding(Float64,RoundDown) do
+                   1.1 + 0.1
+               end
+        1.2000000000000002
+    Here the compiler is *constant folding*, that is evaluating a known constant
+    expression at compile time, however the rounding mode is only changed at runtime, so
+    this is not reflected in the function result. This can be avoided by moving constants
+    outside the expression, e.g.
+        julia> x = 1.1; y = 0.1;
+        julia> setrounding(Float64,RoundDown) do
+                   x + y
+               end
+        1.2
 """
 function setrounding(f::Function, ::Type{T}, rounding::RoundingMode) where T
     old_rounding_raw = rounding_raw(T)
@@ -204,12 +141,10 @@ end
 
 """
     set_zero_subnormals(yes::Bool) -> Bool
-
 If `yes` is `false`, subsequent floating-point operations follow rules for IEEE arithmetic
 on subnormal values ("denormals"). Otherwise, floating-point operations are permitted (but
 not required) to convert subnormal inputs or outputs to zero. Returns `true` unless
 `yes==true` but the hardware does not support zeroing of subnormal numbers.
-
 `set_zero_subnormals(true)` can speed up some computations on some hardware. However, it can
 break identities such as `(x-y==0) == (x==y)`.
 """
@@ -217,14 +152,9 @@ set_zero_subnormals(yes::Bool) = ccall(:jl_set_zero_subnormals,Int32,(Int8,),yes
 
 """
     get_zero_subnormals() -> Bool
-
-Return `false` if operations on subnormal floating-point values ("denormals") obey rules
+Returns `false` if operations on subnormal floating-point values ("denormals") obey rules
 for IEEE arithmetic, and `true` if they might be converted to zeros.
 """
 get_zero_subnormals() = ccall(:jl_get_zero_subnormals,Int32,())!=0
 
 end #module
-
-# package code goes here
-
-end # module
